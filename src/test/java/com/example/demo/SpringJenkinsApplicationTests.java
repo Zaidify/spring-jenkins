007151aede90cdebdfadbfcb32cce16ea9748773
zaidansari05 <zaidbingufran@gmail.com>
2026-01-07 12:06:22 +0530

    Successful

--- src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
     private static final Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
     @Test
-      contextLoads() {
+    public void contextLoads() {
         logger.info("Test case executing...");
         logger.info("Test case executing Second log Statement...");
 
