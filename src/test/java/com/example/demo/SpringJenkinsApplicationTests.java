b98fcca1e1d547eaaa49dcead710f4bd5e13f5d3
zaidansari05 <zaidbingufran@gmail.com>
2026-01-07 11:59:19 +0530

    forcefull

--- src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
     private static final Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
     @Test
-    public void contextLoads() {
+     void contextLoads() {
         logger.info("Test case executing...");
         logger.info("Test case executing Second log Statement...");
 
